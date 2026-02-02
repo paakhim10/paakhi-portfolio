b6f9cb1c5e2bd6e27c9299ef34e8d56e8a3a36d2
paakhim10 <paakhim10@gmail.com>
2026-02-02 13:14:13 +0530

    corrects hearse poem

--- src/articles/hearse.js
+++ src/articles/hearse.js
@@ -8,12 +8,12 @@ const hearse = `
 > *all love long lost, now back on a lease",*  
 > *ah, the cruelly wagered on fleeting kindness,*  
 > *"rest easy, soldier, that herded blindness*  
->
 > *left you alone, bereft and bereaved,*  
 > *yet hope's own daggers left you cleaved."*  
 >
 > *now split remain the ones who remain,*  
 > *silently mourning and silently marching.*  
+>
 > *the hearse of the alive carries no pain,*  
 > *while the flame of the dead lies scorching.*  
  
